cb2b7bcac7099140266df6a3e397fe1de5dd2e20
Sergey Kirsanov <qvanchik571@yandex.ru>
2023-01-30 22:58:21 +0300

    refact:(hotfix): вторая итерация, исправил все критические замечания

--- scripts/script.js
+++ scripts/script.js
@@ -4,44 +4,39 @@ let profileSubtitle = document.querySelector(".profile__subtitle");
 let profileInputTitle = document.querySelector("#name");
 let profileInputSubtitle = document.querySelector("#descrip");
 let popupProfile = document.querySelector(".popup");
+let profileFormElement = popupProfile.querySelector(".popup__form");
+let popupProfileCloseButton = document.querySelector(".popup__profile-close");
+let imageLikeButton = document.querySelectorAll(".photos__button-like");
 
-profileEditButton.addEventListener("click", function () {
+function openPopup() {
     profileInputTitle.value = profileTitle.textContent;
     profileInputSubtitle.value = profileSubtitle.textContent;
-    openPopup("popup_open");
-});
+    popupProfile.classList.add("popup_open");
+};
 
-let profileFormElement = popupProfile.querySelector(".popup__form");
+function closePopup() {
+    popupProfile.classList.remove("popup_open");
+};
 
-profileFormElement.addEventListener("submit", function (e) {
+function saveProfile() {
+    profileTitle.textContent = profileInputTitle.value;
+    profileSubtitle.textContent = profileInputSubtitle.value;
+};
+
+function prevent(e) {
     e.preventDefault();
     saveProfile();
     closePopup("popup_open");
-});
+};
 
-let popupProfileCloseButton = document.querySelector(".popup__profile-close");
+profileEditButton.addEventListener('click', openPopup);
 
-popupProfileCloseButton.addEventListener("click", function() {
-    closePopup("popup_open");
-});
+profileFormElement.addEventListener('submit', prevent);
 
-let imageLikeButton = document.querySelectorAll(".photos__button-like");
+popupProfileCloseButton.addEventListener('click', closePopup);
 
 for (let i = 0; i < imageLikeButton.length; i++) {
     imageLikeButton[i].addEventListener("click", function() {
         return imageLikeButton[i].classList.toggle("photos__button-like_active");
     });
-}
-
-function openPopup(div) {
-    popupProfile.classList.add(div);
-}
-
-function closePopup(div) {
-    popupProfile.classList.remove(div);
-}
-
-function saveProfile() {
-    profileTitle.textContent = profileInputTitle.value;
-    profileSubtitle.textContent = profileInputSubtitle.value;
 }
