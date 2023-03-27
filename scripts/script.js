2090a1f2aafa542f9a68cd40ee2d3c6137a5bc83
Sergey Kirsanov <qvanchik571@yandex.ru>
2023-03-27 13:37:36 +0300

    refact:(hotfix): фих 6-го проекта: исправлена активность кнопки добавления мест

--- scripts/script.js
+++ scripts/script.js
@@ -107,12 +107,14 @@ profileEditButton.addEventListener('click', function () {
   profileInputTitle.value = profileTitle.textContent;
   profileInputSubtitle.value = profileSubtitle.textContent;
   openPopup(popupProfile);
+  enableValidation(settings);
 });
 
 profileFormElement.addEventListener('submit', submitFormProfile);
 
 photoAddButton.addEventListener('click', function() {
   openPopup(popupPhoto);
+  enableValidation(settings);
 });
 
 photoFormElement.addEventListener('submit', function(e) {
